32fbf876a7d9264c71b9277fe55968c993b26c45
utkrista <kcutkrista92@gmail.com>
2018-09-11 11:27:49 +0545

    first commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine','hbs');
+app.use(express.static(__dirname+`/public`));
+
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = `${now}:${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log+'\n',(err)=>{
+    if(err)
+    {
+      console.log('unable to append to server.log');
+    }
+  });
+  next();
+});
+
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+
+app.get('/',(req,res)=>{
+  //res.send('<h1>hello express....</h1>');
+res.render('home.hbs',{
+  pageTitle: 'HOme Page',
+  welcomeMessage: 'Welcome to my blog'
+
+});
+});
+
+app.get('/about',(req,res)=>{
+res.render('about.hbs',{
+  pageTitle: 'About Page'
+
+});
+});
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    errorMessage: 'unable to handle request'
+  });
+});
+
+app.listen(port,()=>{
+  console.log(`server is up on port ${port}`);
+});
